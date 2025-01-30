ecce062c724bf548bc2077055c5827c17566719a
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2025-01-30 16:54:23 +0300

    Update rabbitMQ.js

--- backend/config/rabbitMQ.js
+++ backend/config/rabbitMQ.js
@@ -1,14 +1,22 @@
-const amqp = require('amqplib');
+const useRabbitMQ = process.env.USE_RABBITMQ || "false";
 
-const connectRabbitMQ = async () => {
-    try {
-        const connection = await amqp.connect(process.env.RABBITMQ_URL || 'amqp://localhost');
-        console.log('Connected to RabbitMQ');
-        return connection;
-    } catch (error) {
-        console.error('Failed to connect to RabbitMQ:', error.message);
-        process.exit(1);
-    }
-};
+if (useRabbitMQ === "false") {
+    console.log("🚀 RabbitMQ devre dışı bırakıldı.");
+    module.exports = null;
+} else {
+    const amqp = require("amqplib");
 
-module.exports = connectRabbitMQ;
+    const connectRabbitMQ = async () => {
+        try {
+            const connection = await amqp.connect(process.env.RABBITMQ_URL);
+            const channel = await connection.createChannel();
+            console.log("✅ RabbitMQ connected successfully!");
+            return channel;
+        } catch (error) {
+            console.error("❌ Failed to connect to RabbitMQ:", error);
+            process.exit(1);
+        }
+    };
+
+    module.exports = connectRabbitMQ;
+}
