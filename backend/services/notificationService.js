16a41f170df061115a290a23250053331fc95531
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2025-01-30 17:02:52 +0300

    Update notificationService.js

--- backend/services/notificationService.js
+++ backend/services/notificationService.js
@@ -1,18 +1,12 @@
-const connectRabbitMQ = require('../config/rabbitMQ');
-const sendEmail = require('../utils/email');
-const { Prescription } = require('../models');
-
 const connectRabbitMQ = require("../config/rabbitmq");
 
 const processNotifications = async () => {
     try {
         const { connection, channel } = await connectRabbitMQ();
         console.log("🔔 Notification service connected to RabbitMQ.");
-        // Burada mesaj kuyruğunu işleyecek kodlarını yaz
     } catch (error) {
         console.error("❌ Notification Service Error:", error);
     }
 };
 
 module.exports = processNotifications;
-
